cb42b5c75347f3cddeba756bd6f1bb712a0b0c75
Harshit <harshitpachauri555@gmail.com>
2024-05-20 03:37:34 +0530

    just responsive home and change title and favicon

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react';
 
-const Home = ({toogle}) => {
-  
+const Home = ({ toggle }) => {
   return (
-    <div className='bg-soft-radial h-screen w-screen flex justify-center items-center'>
-        <div className=' flex  h-[80%] w-[80%] border border-black text-black '>
-            {/* left */}
-            <div className=' w-[50%]'>
-                <img className=' h-fit w-fit' src="../Images/Dicess/Dicess.png" alt="" />
-            </div>
-            {/* right */}
-            <div className=' w-[50%] flex flex-col gap-9 justify-center items-center'>
-                <h1 className=' text-8xl font-extrabold'>Dice Game</h1>
-                <button onClick={toogle}  className=' border-2 border-black text-white bg-black p-3 rounded-md   hover:bg-white hover:text-black transition-colors duration-300'>Play Game</button>
-
-            </div>
+    <div className="bg-soft-radial min-h-screen w-full flex justify-center items-center p-4">
+      <div className="flex flex-col lg:flex-row h-full lg:h-[80%] w-full lg:w-[80%] border border-black text-black">
+        {/* left */}
+        <div className="w-full lg:w-[50%] flex justify-center items-center p-4">
+          <img className="max-h-full max-w-full" src="../Images/Dicess/Dicess.png" alt="Dice Game" />
         </div>
+        {/* right */}
+        <div className="w-full lg:w-[50%] flex flex-col gap-6 justify-center items-center p-4">
+          <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold text-center">Dice Game</h1>
+          <button
+            onClick={toggle}
+            className="border-2 border-black text-white bg-black p-3 rounded-md hover:bg-white hover:text-black transition-colors duration-300"
+          >
+            Play Game
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
